ef618b62f49cba1b9b58cc1d2dbe4d3d1db6864a
Alice Chuang <smillaraaq@Alices-MacBook-Pro.local>
2017-06-19 15:52:34 -0400

    connected users page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,30 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+// root route loads tweetBank
 router.get('/', function (req, res) {
     let tweets = tweetBank.list();
     res.render( 'index', { tweets: tweets } );
+});
+
+// users route
+router.get( '/users', function (req, res) {
+    res.send("I'm in USERS");
+});
+
+// say that a client GET requests the path /users/nimit
+router.get( '/users/:name', function (req, res) {
+    console.log("req.params.name",req.params.name); // --> 'nimit'
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { tweets: list } );
+});
+
+router.get( '/store/:product/reviews/:id', function (req, res) {
+    // use req.params here
+    // store/microwave/reviews/197
+    // product=microwave id=197
+    console.log(req.params.product, req.params.id);
 });
 
 // __dirname +
